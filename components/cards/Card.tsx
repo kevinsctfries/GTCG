14c9af590acba9a18a1464c21587fc063ae9ab96
Kevin Fries <kevinsctfries@gmail.com>
2026-07-04 21:21:35 -0700

    feat: add card flipping animation and refactor scss using nesting

--- components/cards/Card.tsx
+++ components/cards/Card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { PackCard } from "@/lib/cards/generator";
 import styles from "@/styles/components/card.module.scss";
 
@@ -8,16 +9,28 @@ type Props = {
 };
 
 export default function Card({ item }: Props) {
-  return (
-    <div className={styles.card}>
-      <div className={styles.name}>{item.card.name}</div>
+  const [flipped, setFlipped] = useState(false);
 
-      <div className={styles.meta}>
-        <span>{item.card.rarity}</span>
-        <span>{item.card.category}</span>
+  return (
+    <div
+      className={`${styles.card} ${flipped ? styles.flipped : ""}`}
+      onClick={() => setFlipped(prev => !prev)}>
+      {/* face down */}
+      <div className={styles.face + " " + styles.back}>
+        <span>GTCG</span>
       </div>
 
-      <div className={styles.description}>{item.card.description}</div>
+      {/* face up */}
+      <div className={styles.face + " " + styles.front}>
+        <div className={styles.name}>{item.card.name}</div>
+
+        <div className={styles.meta}>
+          <span>{item.card.rarity}</span>
+          <span>{item.card.category}</span>
+        </div>
+
+        <div className={styles.description}>{item.card.description}</div>
+      </div>
     </div>
   );
 }
