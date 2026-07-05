a06b9db13ecacbb52687f5a30821aa36e85caccf
Kevin Fries <kevinsctfries@gmail.com>
2026-07-04 23:18:33 -0700

    refactor(card): simplify card UI to face-up only

--- components/cards/Card.tsx
+++ components/cards/Card.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { PackCard } from "@/lib/cards/generator";
 import styles from "@/styles/components/card.module.scss";
 
@@ -9,28 +8,16 @@ type Props = {
 };
 
 export default function Card({ item }: Props) {
-  const [flipped, setFlipped] = useState(false);
-
   return (
-    <div
-      className={`${styles.card} ${flipped ? styles.flipped : ""}`}
-      onClick={() => setFlipped(prev => !prev)}>
-      {/* face down */}
-      <div className={styles.face + " " + styles.back}>
-        <span>GTCG</span>
+    <div className={styles.card}>
+      <div className={styles.name}>{item.card.name}</div>
+
+      <div className={styles.meta}>
+        <span>{item.card.rarity}</span>
+        <span>{item.card.category}</span>
       </div>
 
-      {/* face up */}
-      <div className={styles.face + " " + styles.front}>
-        <div className={styles.name}>{item.card.name}</div>
-
-        <div className={styles.meta}>
-          <span>{item.card.rarity}</span>
-          <span>{item.card.category}</span>
-        </div>
-
-        <div className={styles.description}>{item.card.description}</div>
-      </div>
+      <div className={styles.description}>{item.card.description}</div>
     </div>
   );
 }
